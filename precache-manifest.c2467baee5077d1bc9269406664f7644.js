64f371f90edc7659a29b4ee30f74cecaa00d826a
Javier Machin <javier_machin_acosta@hotmail.com>
2019-01-28 13:21:40 +0000

    Updates

---
+++ precache-manifest.c2467baee5077d1bc9269406664f7644.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44adf1227610ce0e0a40",
+    "url": "/react-videos/static/js/runtime~main.44adf122.js"
+  },
+  {
+    "revision": "94f61968032f21b9dac8",
+    "url": "/react-videos/static/js/main.94f61968.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/react-videos/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "94f61968032f21b9dac8",
+    "url": "/react-videos/static/css/main.2e1dec5b.chunk.css"
+  },
+  {
+    "revision": "9520e506e8c1c19a7dc764a933b06d50",
+    "url": "/react-videos/index.html"
+  }
+];
